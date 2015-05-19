8b358763dcb6aff30396fceafe98f180cb85443b
Ben Clayton <ben@calvium.com>
2015-05-19 16:56:44 +0100

    remove some unused code

--- RealmTableTest/RealmTableTest/Model/SubItem.m
+++ RealmTableTest/RealmTableTest/Model/SubItem.m
@@ -9,19 +9,4 @@
     return @"id";
 }
 
-//- (NSString *)parentId {
-//    NSArray *array = [self linkingObjectsOfClass:[Item className]
-//                                     forProperty:@"subitems"];
-//
-//    if (!array.count) {
-//        return nil;
-//    }
-//    Item *parent = array.firstObject;
-//    return parent.id;
-//}
-
-- (NSArray *)parents {
-    return [self linkingObjectsOfClass:[Item className] forProperty:@"subitems"];
-}
-
 @end
